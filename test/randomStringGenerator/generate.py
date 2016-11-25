a8eb52818f8620419f1759add7a571e09ff2871e
chaitanya0411 <chaitanyakulkarni0411@gmail.com>
2016-11-24 22:29:17 -0500

    fixed variable names

--- test/randomStringGenerator/generate.py
+++ test/randomStringGenerator/generate.py
@@ -3,16 +3,16 @@ import sys
 import random
 
 f1 = open(str(sys.argv[1]), 'w')
-n = int(str(sys.argv[2]))
-l = int(str(sys.argv[3]))
+number_of_strings = int(str(sys.argv[2]))
+length_of_string = int(str(sys.argv[3]))
 
 nucleotides = ['A', 'C', 'G', 'T']
 
-for x in range(1, n):
+for x in range(1, number_of_strings):
     line = ""
-    for y in range(1, l):
-        randomNo = random.randint(0, 3)
-        line += nucleotides[randomNo]
+    for y in range(1, length_of_string):
+        random_no = random.randint(0, 3)
+        line += nucleotides[random_no]
     line += "\n"
     f1.write(line)
 
